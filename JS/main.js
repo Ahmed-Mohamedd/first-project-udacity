ecec277fe0d7c4b7c455866cc3f46ab0cd1a5034
Ahmed Mohamed <aahmedfarhatt@yahoo.com>
2021-09-23 19:16:52 +0200

    second commit

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,13 @@
+
+// selectors 
 const mySections = document.querySelectorAll("section");
 const myUl = document.querySelector("ul");
 
 
-
-
-
+/* create navigation dynamically 
+*  By using 'create element ' to create lists and links
+*  and use apppendChild to add them to an empty ul 
+*/
 
 for(section of mySections){
     let myList = document.createElement("li");
@@ -13,12 +16,14 @@ for(section of mySections){
     myList.appendChild(myLink);
     myUl.appendChild(myList);
 }
+ // adding new atribute to 'a'
 let a = document.querySelectorAll("a");
 a[0].setAttribute("data-link","section1") ;
 a[1].setAttribute("data-link","section2") ;
 a[2].setAttribute("data-link","section3") ;
 a[3].setAttribute("data-link","section4") ;
 
+// making loop in all links to achieve smooth scroll for page's sections 
 a.forEach((link) => {
     link.addEventListener("click", function () {
         const element = document.getElementById(link.getAttribute("data-link"));
@@ -30,6 +35,8 @@ a.forEach((link) => {
     });
 });
 
+// function to observe the section to specify which section on viewport
+
 window.addEventListener("scroll", () => {
     let myPosition = "";
     mySections.forEach((section) => {
@@ -39,12 +46,15 @@ window.addEventListener("scroll", () => {
         myPosition = section.getAttribute("id");
         }
     });
+    // get actice link by using the id 
+    // add active class to the class on viewport
     mySections.forEach((section) => {
         section.classList.remove("your-active-class");
         if (myPosition === section.getAttribute("id")) {
         document.getElementById(myPosition).classList.add("your-active-class");
         }
     });
+    // add active link to section's link
     a.forEach((link) => {
         link.classList.remove("activeLink");
         if (myPosition === link.getAttribute("data-link")) {
@@ -53,6 +63,12 @@ window.addEventListener("scroll", () => {
     });
 });
 
+/* creste button and append it sectio1
+*  using create element and append child 
+*  naming button 'Back to Top' using innerhtml 
+*  making function to specify where button should appear ?
+*  making function to achieve smooth scroll. 
+*/
 document
     .getElementById("section1")
     .appendChild(document.createElement("button"));
